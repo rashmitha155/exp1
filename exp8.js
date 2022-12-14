2c104950e175c41c1ea2b86fb119c94fbb2acc78
radha <21b01a1263@svecw.edu.in>
2022-12-14 11:28:12 +0530

    first commit

---
+++ exp8.js
@@ -0,0 +1,13 @@
+function add(x,y){
+    return x+y
+}
+console.log("addition:"+add(5,15))
+function area(l,b){
+    return l*b
+}
+console.log("area of rectangle:"+area(3,4))
+function intosec(min){
+    let sec=(min*60)
+    return sec
+}
+console.log("convert min into sec:"+intosec(1))
